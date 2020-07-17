bd07a9e7056233f346562426795ce8f5d9dc19ce
Sudhir Dadwal <sudhir.dadwal@gmail.com>
2020-07-17 14:45:52 +0530

    checking the  test case

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -4,17 +4,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-/*it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-  });
-}); */
 const wrapper = shallow(<App />);
 it('should have input for Search', () => {
   //Search input field should be present
